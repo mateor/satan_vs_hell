cab3f7a7eb9c9420c89c750ffc6e5228266f4b82
mateor <mateorod9@gmail.com>
2020-09-05 19:46:45 -0400

    game state

--- js/game.js
+++ js/game.js
@@ -24,6 +24,7 @@ function drawSprite(sprite, x, y){
 }
 
 function draw(){
+  if(gameState == "running" || gameState == "dead"){  
     ctx.clearRect(0,0,canvas.width,canvas.height);
     for(let i=0;i<numTiles;i++){
       for(let j=0;j<numTiles;j++){
@@ -34,14 +35,39 @@ function draw(){
         monsters[i].draw();
     }
     player.draw();
+  }
 }
 
 function tick(){
-    for(let k=monsters.length-1;k>=0;k--){
-        if(!monsters[k].dead){
-            monsters[k].update();
-        }else{
-            monsters.splice(k,1);
-        }
+  for(let k=monsters.length-1;k>=0;k--){
+    if(!monsters[k].dead){
+      monsters[k].update();
+    }else{
+      monsters.splice(k,1);
     }
+  }
+  if(player.dead){    
+    gameState = "dead";
+  }
+}
+
+function showTitle(){                                          
+    ctx.fillStyle = 'rgba(0,0,0,.75)';
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+
+    gameState = "title";
+}
+
+function startGame(){                                           
+    level = 1;
+    startLevel(startingHp);
+
+    gameState = "running";
+}
+
+function startLevel(playerHp){                          
+    generateLevel();
+
+    player = new Player(randomPassableTile());
+    player.hp = playerHp;
 }
